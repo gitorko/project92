88a85ef9a7dd77fecb85eb58001ee7608fa37159
Arjun Surendra <asurendra@vmware.com>
2021-11-11 14:01:52 +0530

    code

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {WebsocketService} from "../services/websocket.service";
 import {Chat} from "../models/chat";
+import * as SockJS from "sockjs-client";
+import {Stomp} from "@stomp/stompjs";
 
 @Component({
   selector: 'app-home',
@@ -12,29 +13,53 @@ export class HomeComponent implements OnInit {
   message: string = '';
   chatStatus: string = 'Disconnected';
 
-  constructor(public websocketService: WebsocketService) {
+  stompClient: any;
+  public messageList: Chat[] = [];
+
+  constructor() {
   }
 
   ngOnInit(): void {
   }
 
   connect() {
-    this.websocketService._connect();
+    console.log(window.location.href);
+    const serverUrl = 'http://localhost:8080/chat-app';
+    const ws = new SockJS(serverUrl);
+    this.stompClient = Stomp.over(ws);
+    const that = this;
+    this.stompClient.connect({}, function (frame: any) {
+      that.stompClient.subscribe('/message', (message: any) => {
+        if (message.body) {
+          that.messageList.push(JSON.parse(message.body));
+        }
+      });
+    }, this.errorCallBack);
     this.chatStatus = 'Connected';
   }
 
   disconnect() {
-    this.websocketService._disconnect();
-    this.chatStatus = 'Disconnected';
+    if (this.stompClient !== null) {
+      this.stompClient.disconnect();
+      this.chatStatus = 'Disconnected';
+    }
+    console.log("Disconnected");
   }
 
   sendMessage() {
     if (this.message) {
       let chat: Chat = new Chat();
       chat.text = this.message;
-      this.websocketService._send(chat);
+      console.log("Sending chat: " + chat);
+      this.stompClient.send('/app/send/message', {}, JSON.stringify(chat));
       this.message = '';
     }
   }
 
+  errorCallBack(error: any) {
+    console.log("errorCallBack -> " + error)
+    setTimeout(() => {
+      this.connect();
+    }, 5000);
+  }
 }
